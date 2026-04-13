f207f6a03b1b31938be2dd317b8184546d3343b0
Kevin <kevinrosenov80@gmail.com>
2026-04-14 01:09:51 +0200

    espacio

--- src/joc/Alien.java
+++ src/joc/Alien.java
@@ -4,6 +4,6 @@ public class Alien extends Player {
 
     public Alien(String name, int attack, int defense, int life) {
         super(name, attack, defense, life);
-        System.out.println("Creo Alien ");
+        System.out.println("Creo Alien  ");
     }
 }
